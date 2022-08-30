24fe5c1aec7df8084ebfd1d867110d2ef6aa0dee
Jacob Michels <jacob.michels2025@gmail.com>
2022-08-30 19:21:33 -0400

    add config defaults

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,18 +1,32 @@
 package config
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/spf13/viper"
 )
 
 func Init() error {
 	viper.SetDefault("discord.token", "")
+	viper.SetDefault("rcon.address", "")
+	viper.SetDefault("rcon.password", "minecraft") // mc's default rcon password
 	viper.AddConfigPath(".")
 	viper.SetConfigName("warden_config")
+	viper.SetEnvPrefix("WARDEN")
+	viper.AutomaticEnv()
+
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
 
 	if err := viper.ReadInConfig(); err != nil {
-		return fmt.Errorf("viper failed to read in config: %w", err)
+		if ok := errors.As(err, &viper.ConfigFileNotFoundError{}); !ok {
+			return fmt.Errorf("viper failed to read in config: %w", err)
+		}
+
+		log.Println("Config file not found, using defaults and env")
 	}
 
 	return nil
